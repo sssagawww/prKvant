61f4aaaf37954ba04fa4ead82244ad6d33282e63
sssagawww <eta19777@gmail.com>
2022-10-31 12:42:31 +0300

    add

--- src/main/java/com/example/prkvant/Raspisanie.java
+++ src/main/java/com/example/prkvant/Raspisanie.java
@@ -63,6 +63,7 @@ public class Raspisanie {
     @FXML
     void initialize() {
         //https://stackoverflow.com/questions/32349910/javafx-8-setvisible-is-blocked
+        //https://overcoder.net/q/2903031/%D0%BA%D0%B0%D0%BA-%D0%B7%D0%B0%D0%BF%D1%83%D1%81%D1%82%D0%B8%D1%82%D1%8C-%D0%BE%D0%B4%D0%B8%D0%BD-%D0%BF%D0%BE%D1%82%D0%BE%D0%BA-%D0%B4%D0%BB%D1%8F-%D0%BC%D0%BE%D0%B5%D0%B3%D0%BE-%D0%BA%D0%BE%D0%B4%D0%B0-%D0%B8-%D0%BE%D0%B4%D0%B8%D0%BD-%D0%B4%D0%BB%D1%8F-%D0%BF%D1%80%D0%B8%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D1%8F-javafx
         branchItem1_1.getChildren().addAll(leafItem1_1_1);
         branchItem2_1.getChildren().addAll(leafItem2_1_1, leafItem2_1_2, leafItem2_1_3);
 
