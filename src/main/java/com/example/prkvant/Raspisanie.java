d851c2182cc834e4506f1ac862de6ae28b703644
sssagawww <eta19777@gmail.com>
2022-11-19 14:27:17 +0300

    add new

--- src/main/java/com/example/prkvant/Raspisanie.java
+++ src/main/java/com/example/prkvant/Raspisanie.java
@@ -24,17 +24,9 @@ public class Raspisanie {
     public ResourceBundle resources;
     public URL location;
 
-    /*public Image image211 = new Image("E:/IdeaProjects/prKvant/src/main/resources/com/example/prkvant/img1.jpg");
-    public Image image212 = new Image("E:/IdeaProjects/prKvant/src/main/resources/com/example/prkvant/img2.png");
-    public Image image213 = new Image("E:/IdeaProjects/prKvant/src/main/resources/com/example/prkvant/img3.jpg");*/
-
-    //public TreeItem<String>[] treeItems = new TreeItem[3];
-
     public TreeItem<String> rootItem1 = new TreeItem<>("Расписание");
     public TreeItem<String> branchItem1_1 = new TreeItem<>("Java");
     public TreeItem<String> leafItem1_1_1 = new TreeItem<>("Java 2.1");
-
-    //public TreeItem<String> mainRoot = new TreeItem<>("не нужная строка");
 
     public TreeItem<String> rootItem2 = new TreeItem<>("События");
     public TreeItem<String> branchItem2_1 = new TreeItem<>("Ноябрь");
@@ -80,6 +72,8 @@ public class Raspisanie {
     private Pane tablePane;
     @FXML
     private TextArea txArea;
+    /*@FXML
+    private TextArea txArea2;*/
 
     public BufferedReader br = null;
     public File file = new File("Val.txt");
@@ -98,7 +92,6 @@ public class Raspisanie {
     );
     @FXML
     void initialize() {
-        //https://www.youtube.com/watch?v=3s8s0vyfLV8
         branchItem1_1.getChildren().addAll(leafItem1_1_1);
         branchItem2_1.getChildren().addAll(leafItem2_1_1, leafItem2_1_2, leafItem2_1_3);
 
@@ -139,16 +132,27 @@ public class Raspisanie {
         String currentDate = dateInp.getText();
         for (TableRasp2 tableRasp : currentData) {
             if (tableRasp.getColumn().equals(currentDate)) {
-                //txArea.setText();
-                line = (String.valueOf(txArea)); // !!!
+               /* pw = new PrintWriter(file);
+                br = new BufferedReader(new FileReader("Val.txt"));
+                if (line == null) {
+                    pw.println(txArea.getText());
+                    while ((line = br.readLine()) != null){
+                        System.out.println(":)");
+                    }
+                } else {
+                    System.out.println(":O");
+                    txArea.setText(line);
+                }
+                pw.close();
+                br.close();*/
                 pw = new PrintWriter(file);
-                pw.println(line);
+                pw.println(txArea.getText());
+                pw.close();
                 br = new BufferedReader(new FileReader("Val.txt"));
                 while ((line = br.readLine()) != null){
-                    pw.println(":)");
+                   System.out.println(":)");
                 }
                 br.close();
-                pw.close();
 
                 tableRasp.setDow(dowInp.getText());
                 tableRasp.setTime(timeInp.getText());
@@ -162,9 +166,13 @@ public class Raspisanie {
     @FXML
     void rowClicked (MouseEvent event){
         TableRasp2 tableRaspclick = table.getSelectionModel().getSelectedItem();
-        dateInp.setText(String.valueOf(tableRaspclick.getColumn()));
-        dowInp.setText(String.valueOf(tableRaspclick.getDow()));
-        timeInp.setText(String.valueOf(tableRaspclick.getTime()));
+        if (tableRaspclick == null) {
+            System.out.println("null tableRaspclick");
+        } else {
+            dateInp.setText(String.valueOf(tableRaspclick.getColumn()));
+            dowInp.setText(String.valueOf(tableRaspclick.getDow()));
+            timeInp.setText(String.valueOf(tableRaspclick.getTime()));
+        }
     }
 
     public void setTableColumn(URL url, ResourceBundle resourceBundle){
